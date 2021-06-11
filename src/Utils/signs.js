c7157aeaa31944cae4552971c5936a93dcc8a9a1
EllieAzaveda <elliemiller66@icloud.com>
2021-06-10 21:14:03 -0600

    Add array of objects for dropdown

---
+++ src/Utils/signs.js
@@ -0,0 +1,62 @@
+export const signs = [
+  {
+    name: 'Aries',
+    date_range: 'Mar 21 – Apr 19',
+    img_path: ''
+  },
+  {
+    name: 'Taurus',
+    date_range: 'Apr 20 – May 20',
+    img_path: ''
+  },
+  {
+    name: 'Gemini',
+    date_range: 'May 21 – Jun 21',
+    img_path: ''
+  },
+  {
+    name: 'Cancer',
+    date_range: 'Jun 22 – Jul 22',
+    img_path: ''
+  },
+  {
+    name: 'Leo',
+    date_range: 'Jul 23 – Aug 22',
+    img_path: ''
+  },
+  {
+    name: 'Virgo',
+    date_range: 'Aug 23 – Sep 22',
+    img_path: ''
+  },
+  {
+    name: 'Libra',
+    date_range: 'Sep 23 – Oct 23',
+    img_path: ''
+  },
+  {
+    name: 'Scorpio',
+    date_range: 'Oct 24 – Nov 21',
+    img_path: ''
+  },
+  {
+    name: 'Sagittarius',
+    date_range: 'Nov 22 – Dec 21',
+    img_path: ''
+  },
+  {
+    name: 'Capricorn',
+    date_range: 'Dec 22 – Jan 19',
+    img_path: ''
+  },
+  {
+    name: 'Aquarius',
+    date_range: 'Jan 20 – Feb 18',
+    img_path: ''
+  },
+  {
+    name: 'Pisces',
+    date_range: 'Feb 19 – Mar 20',
+    img_path: ''
+  },
+];
