8ec6c61477b859fa81406f477bbcdd217ab777b0
EllieAzaveda <elliemiller66@icloud.com>
2021-06-10 22:49:02 -0600

    Add image paths to objects

--- src/Utils/signs.js
+++ src/Utils/signs.js
@@ -2,61 +2,61 @@ export const signs = [
   {
     name: 'Aries',
     date_range: 'Mar 21 – Apr 19',
-    img_path: ''
+    img_path: 'Aries.png'
   },
   {
     name: 'Taurus',
     date_range: 'Apr 20 – May 20',
-    img_path: ''
+    img_path: 'Taurus.png'
   },
   {
     name: 'Gemini',
     date_range: 'May 21 – Jun 21',
-    img_path: ''
+    img_path: 'Gemini.png'
   },
   {
     name: 'Cancer',
     date_range: 'Jun 22 – Jul 22',
-    img_path: ''
+    img_path: 'Cancer.png'
   },
   {
     name: 'Leo',
     date_range: 'Jul 23 – Aug 22',
-    img_path: ''
+    img_path: 'Leo.png'
   },
   {
     name: 'Virgo',
     date_range: 'Aug 23 – Sep 22',
-    img_path: ''
+    img_path: 'Virgo.png'
   },
   {
     name: 'Libra',
     date_range: 'Sep 23 – Oct 23',
-    img_path: ''
+    img_path: 'Libra.png'
   },
   {
     name: 'Scorpio',
     date_range: 'Oct 24 – Nov 21',
-    img_path: ''
+    img_path: 'Scorpio.png'
   },
   {
     name: 'Sagittarius',
     date_range: 'Nov 22 – Dec 21',
-    img_path: ''
+    img_path: 'Sagittarius.png'
   },
   {
     name: 'Capricorn',
     date_range: 'Dec 22 – Jan 19',
-    img_path: ''
+    img_path: 'Capricorn.png'
   },
   {
     name: 'Aquarius',
     date_range: 'Jan 20 – Feb 18',
-    img_path: ''
+    img_path: 'Aquarius.png'
   },
   {
     name: 'Pisces',
     date_range: 'Feb 19 – Mar 20',
-    img_path: ''
+    img_path: 'Pisces.png'
   },
 ];
