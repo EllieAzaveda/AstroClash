68ed6cfeaf9a6640b687b9b27aa9b434eeadaac5
EllieAzaveda <elliemiller66@icloud.com>
2021-06-10 21:34:46 -0600

    Fix selected sing property

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,19 +2,29 @@ import React, { Component } from 'react';
 import Header from '../Header/Header';
 import Astrology from '../Astrology/Astrology';
 import Astronomy from '../Astronomy/Astronomy';
+import Form from '../Form/Form';
+// import { signs } from './../../Utils/signs.js'
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
       this.state = {
-
+        selectedSign: null,
       }
+  }
+
+  setZodiacSign = (sign) => {
+    this.setState({ selectedSign: sign })
   }
 
   render() {
     return (
       <>
         <Header />
+        <Form
+          setZodiacSign={this.setZodiacSign}
+        />
         <Astrology />
         <Astronomy />
       </>
