17972755fcfae44f9b5c43384f39ab60ef0d38da
EllieAzaveda <elliemiller66@icloud.com>
2021-06-10 22:46:58 -0600

    Add image

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,7 +25,9 @@ class App extends Component {
         <Form
           setZodiacSign={this.setZodiacSign}
         />
-        <Astrology />
+        <Astrology
+          selectedSign={this.selectedSign}
+        />
         <Astronomy />
       </>
     )
