8f3682b2bbb917413c60033e174c7a47bbc6d58e
EllieAzaveda <elliemiller66@icloud.com>
2021-06-11 18:42:51 -0600

    Fix set sign dropdown

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,34 +1,49 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Astrology from '../Astrology/Astrology';
 import Astronomy from '../Astronomy/Astronomy';
 import Form from '../Form/Form';
-// import { signs } from './../../Utils/signs.js'
+import { signs } from './../../Utils/signs.js'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
       this.state = {
+        allSigns: signs,
         selectedSign: null,
+        isClicked: false
       }
   }
 
   setZodiacSign = (sign) => {
-    this.setState({ selectedSign: sign })
-  }
+    let foundSign = this.state.allSigns.find(zodiac => zodiac.name === sign);
+    this.setState({ selectedSign: foundSign });
+    this.setState({ isClicked: true });
+  };
 
   render() {
     return (
       <>
-        <Header />
-        <Form
-          setZodiacSign={this.setZodiacSign}
-        />
-        <Astrology
-          selectedSign={this.selectedSign}
-        />
-        <Astronomy />
+        <article className='App'>
+          <Header />
+          <Route exact path="/" render={() => {
+            return <Form setZodiacSign={this.setZodiacSign} />
+            }}
+          />
+          <Route exact path="/" render={() => {
+            return <Astrology
+              selectedSign={this.state.selectedSign}
+              isClicked={this.state.isClicked}
+            />
+            }}
+          />
+          <Route exact path="/" render={() => {
+            return <Astronomy />
+            }}
+          />
+        </article>
       </>
     )
   }
