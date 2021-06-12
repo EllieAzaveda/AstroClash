37757e5bc7b91850d11a04ff4454f4632b200af2
EllieAzaveda <elliemiller66@icloud.com>
2021-06-11 18:45:44 -0600

    Add horoscope display

--- src/Components/HoroscopeCard/HoroscopeCard.js
+++ src/Components/HoroscopeCard/HoroscopeCard.js
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { fetchSingleHoroscope } from '../../Utils/APICalls';
 import './HoroscopeCard.css';
 
-const HoroscopeCard = ({ selectedSign }) => {
-  return (
-    console.log("Astrology", selectedSign)
-    let signImg = signs[4].img_path
+class HoroscopeCard extends Component {
+  constructor(props){
+      super(props);
+        this.state = {
+          signData: props.selectedSign,
+          signName: props.selectedSign.name,
+          horoscopeData: {},
+          error: ''
+        }
+  }
 
-      return (
-        <div className='astrology-card'>
-          <img className='astrology-image' src={`${signImg}`} alt='Leo'/>
-          <h4>Compatibility: {this.state.horoscopeData.compatibility} </h4>
-          <h4>Lucky Number: {this.state.horoscopeData.lucky_number} </h4>
-          <h4>Lucky Time: {this.state.horoscopeData.lucky_time} </h4>
-          <h4>Color: {this.state.horoscopeData.color} </h4>
-          <h4>Date Range: {this.state.horoscopeData.date_range} </h4>
-          <h4>Mood: {this.state.horoscopeData.mood} </h4>
-          <h4>Description: {this.state.horoscopeData.description} </h4>
-        </div>
-      );
-  )
+  componentDidMount () {
+    fetchSingleHoroscope(this.state.signName)
+      .then(singleHoroscope => {
+        if(typeof singleHoroscope === 'string') {
+          this.setState({ error: singleHoroscope })
+        } else {
+          this.setState({ horoscopeData: singleHoroscope })
+        }
+    })
+      .catch(err => err.message)
+  }
+
+  render() {
+    return (
+      <div className='astrology-card'>
+        <h4>{this.state.signName} </h4>
+        <img className='astrology-image' src={`${this.state.signData.img_path}`} alt='Leo'/>
+        <h4>Compatibility: {this.state.horoscopeData.compatibility} </h4>
+        <h4>Lucky Number: {this.state.horoscopeData.lucky_number} </h4>
+        <h4>Lucky Time: {this.state.horoscopeData.lucky_time} </h4>
+        <h4>Color: {this.state.horoscopeData.color} </h4>
+        <h4>Date Range: {this.state.horoscopeData.date_range} </h4>
+        <h4>Mood: {this.state.horoscopeData.mood} </h4>
+        <h4>Description: {this.state.horoscopeData.description} </h4>
+      </div>
+    )
+  }
 }
 
 export default HoroscopeCard;
