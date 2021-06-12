6c0b7db8fb4cc454927689074b779d0f8a191356
EllieAzaveda <elliemiller66@icloud.com>
2021-06-11 21:57:17 -0600

    Add save fact method and add to onclick

--- src/Components/AstronomyFact/AstronomyFact.js
+++ src/Components/AstronomyFact/AstronomyFact.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './AstronomyFact.css';
 
-const AstronomyFact = ({ dailyFact }) => {
+const AstronomyFact = ({ dailyFact, saveFact }) => {
   return (
     <div className='astronomy-card'>
       <h4>{dailyFact.title}</h4>
       <img className='astronomy-image' src={dailyFact.url} alt={`${dailyFact.title}`}/>
       <h4>{dailyFact.explanation}</h4>
       <h4>{dailyFact.copyright}</h4>
+      <button className='save-button' onClick={(event) => saveFact(dailyFact)}>Save Fact</button>
     </div>
   )
 }
