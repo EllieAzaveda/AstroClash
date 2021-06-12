00a77ea4b49e99c8e21b07b92ddf79fb4de4d07c
EllieAzaveda <elliemiller66@icloud.com>
2021-06-12 12:42:06 -0600

    Add starter class component

--- src/Components/SavedFacts/SavedFacts.js
+++ src/Components/SavedFacts/SavedFacts.js
@@ -1,48 +1,35 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import SavedCard from '../SavedFacts/SavedFacts';
 import './SavedFacts.css';
 
 class SavedFacts extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        savedFacts: props.savedFacts
+        savedFacts: props.getSavedFacts()
       }
   }
 
-  getSavedFacts = () => {
-    let retrievedFacts = localStorage.getItem("savedFacts");
-    this.setState({ savedFacts: JSON.parse(retrievedFacts)})
-    console.log(this.state.savedFacts)
-  }
-
-  displaySavedFacts = () => {
+  displayCards = () => {
+    console.log("savedFacts", this.state.savedFacts)
     return this.state.savedFacts.map(fact => {
       return (
-        <div className='astronomy-card'>
-          <h4>{fact.date}</h4>
-          <h4>{fact.title}</h4>
-          <img className='astronomy-image' src={fact.url} alt={`${fact.title}`}/>
-          <h4>{fact.explanation}</h4>
-          <h4>{fact.copyright}</h4>
-        </div>
+        <SavedCard
+          date={fact.date}
+          image={fact.url}
+          description={fact.explanation}
+        />
       )
     })
   }
 
   render() {
     return (
-      <>
       <div className='saved-facts-display'>
         <NavLink to='/'><button data-cy='home-button' className='home-button'>Back to Home Page</button></NavLink>
-          {!this.state.savedFacts.length &&
-            <h4>💫 No Saved Facts 💫</h4>
-          }
-          {this.state.savedFacts.length > 0 &&
-              <h1>yay length</h1>
-          }
+        {this.displayCards()}
       </div>
-      </>
     )
   }
 }
