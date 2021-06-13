6572ee46c15cd814ef135aa154064f676ae68ea3
EllieAzaveda <elliemiller66@icloud.com>
2021-06-13 17:58:42 -0600

    Add header titles test

--- cypress/integration/astro_clash_spec.js
+++ cypress/integration/astro_clash_spec.js
@@ -8,4 +8,9 @@ describe('Show main view of AstroClash App', () => {
     cy.url().should('eq', 'http://localhost:3000/')
   });
 
+  it('Should display the title, and subtitle of the page upon loading', () => {
+    cy.contains('nav>h1', 'AstroClash')
+      .get('nav .subtitle').should('contain', 'WHERE YOU DON\'T NEED TO KNOW THE DIFFERENCE BETWEEN ASTRONOMY & ASTROLOGY')
+  });
+
 })
