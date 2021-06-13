8413a44524d2740df768bf79fc62e49185e7dfe7
EllieAzaveda <elliemiller66@icloud.com>
2021-06-12 22:44:15 -0600

    Fix for to htmlFor

--- src/Components/AstronomyFact/AstronomyFact.js
+++ src/Components/AstronomyFact/AstronomyFact.js
@@ -7,12 +7,12 @@ const AstronomyFact = ({ dailyFact, saveFact }) => {
       <img className='astronomy-image' src={dailyFact.url} alt={`${dailyFact.title}`}/>
       <h3 className='title'>..| {dailyFact.title} |..</h3>
       <h5 className='explanation'>{dailyFact.explanation}</h5>
-      <h5>{dailyFact.copyright}</h5>
+      <h5>Copyright: {dailyFact.copyright}</h5>
       <h5 className='save-feature'>
         <button className='save-button'>
           <img className='save-button-img'src='save-img.png' alt='save button' onClick={(event) => saveFact(dailyFact)}/>
         </button>
-        <label for='save-button'>Save this Fact</label>
+        <label htmlFor='save-button'>Save this Fact</label>
       </h5>
     </div>
   )
