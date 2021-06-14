de116653cf98261077634a042976c5e89667431b
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 01:20:37 -0600

    Add conditional rendering for error handling

--- src/Components/HoroscopeCard/HoroscopeCard.js
+++ src/Components/HoroscopeCard/HoroscopeCard.js
@@ -10,7 +10,7 @@ class HoroscopeCard extends Component {
           signData: props.selectedSign,
           signName: props.selectedSign.name,
           horoscopeData: {},
-          error: props.error
+          error: ''
         }
   }
 
@@ -28,21 +28,28 @@ class HoroscopeCard extends Component {
 
   render() {
     return (
-      <div className='astrology-card'>
-        <img className='astrology-image' src={`${this.state.signData.img_path}`} alt='Leo'/>
-        <h3 className='title'>..| {this.state.signName} |..</h3>
-        <h5 data-cy='daily-scope'>Daily Horoscope: {this.state.horoscopeData.description} </h5>
-        <h5 data-cy='mood'>Mood: {this.state.horoscopeData.mood} </h5>
-        <h5 data-cy='lucky-num'>Lucky Number: {this.state.horoscopeData.lucky_number} </h5>
-        <h5 data-cy='color'>Color: {this.state.horoscopeData.color} </h5>
-        <h5 data-cy='compatibility'>Love Compatibility: {this.state.horoscopeData.compatibility} </h5>
-        <h5 className='save-feature'>
-          <button data-cy='save-button' className='save-button'>
-            <img className='save-button-img' src='save-img.png' alt='save button' onClick={(event) => this.props.saveScope(this.state.horoscopeData)}/>
-          </button>
-          <label htmlFor='save-button'>Save this Horoscope</label>
-        </h5>
-      </div>
+      <>
+      {this.state.error &&
+          <h2>{this.state.error}</h2>
+      }
+      {!this.state.error &&
+          <div className='astrology-card'>
+            <img className='astrology-image' src={`${this.state.signData.img_path}`} alt='Leo'/>
+            <h3 className='title'>..| {this.state.signName} |..</h3>
+            <h5 data-cy='daily-scope'>Daily Horoscope: {this.state.horoscopeData.description} </h5>
+            <h5 data-cy='mood'>Mood: {this.state.horoscopeData.mood} </h5>
+            <h5 data-cy='lucky-num'>Lucky Number: {this.state.horoscopeData.lucky_number} </h5>
+            <h5 data-cy='color'>Color: {this.state.horoscopeData.color} </h5>
+            <h5 data-cy='compatibility'>Love Compatibility: {this.state.horoscopeData.compatibility} </h5>
+            <h5 className='save-feature'>
+              <button data-cy='save-button' className='save-button'>
+                <img className='save-button-img' src='save-img.png' alt='save button' onClick={(event) => this.props.saveScope(this.state.horoscopeData)}/>
+              </button>
+              <label htmlFor='save-button'>Save this Horoscope</label>
+            </h5>
+          </div>
+      }
+      </>
     )
   }
 }
