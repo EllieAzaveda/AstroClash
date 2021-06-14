473fbf4349318cea413308b7941ddbbaa8b10f2e
EllieAzaveda <elliemiller66@icloud.com>
2021-06-13 18:10:03 -0600

    Fix form test

--- cypress/integration/astro_clash_spec.js
+++ cypress/integration/astro_clash_spec.js
@@ -8,12 +8,12 @@ describe('Show main view of AstroClash App', () => {
     cy.url().should('eq', 'http://localhost:3000/')
   });
 
-  it('Should display the title, and subtitle of the page upon loading', () => {
+  it('Should display the title and subtitle of the page upon loading', () => {
     cy.contains('nav>h1', 'AstroClash')
       .get('nav .subtitle').should('contain', 'WHERE YOU DON\'T NEED TO KNOW THE DIFFERENCE BETWEEN ASTRONOMY & ASTROLOGY')
   });
 
-  it('Should display the search bar button, and input on the main page', () => {
+  it('Should display the dropdown and corresponding button, and saved items buttons', () => {
     cy.get('form').find('.sign-dropdown').should('be.visible')
       .get('form').find('[data-cy=submit-button]').should('be.visible')
       .get('form').find('[href="/saved-astronomy-facts"] > .saved-button > .moon-icon').should('be.visible')
