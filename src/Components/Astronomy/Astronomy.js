947d5671927878e72a8f9827ddd40f05248dc3a8
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 02:13:58 -0600

    Add additional conditional rendering

--- src/Components/Astronomy/Astronomy.js
+++ src/Components/Astronomy/Astronomy.js
@@ -5,50 +5,50 @@ import { cleanAstronomyData } from '../../Utils/cleaningMethods';
 import './Astronomy.css';
 
 class Astronomy extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          dailyFact: {},
-          isClicked: props.isClicked,
-          error: ''
-        }
-    }
+  constructor(props){
+      super(props);
+      this.state = {
+        dailyFact: {},
+        isClicked: props.isClicked,
+        error: ''
+      }
+  }
 
-    componentDidMount () {
-      fetchDailyFact()
-        .then(dailyFact => {
-          if(typeof dailyFact === 'string') {
-            this.setState({ error: dailyFact })
-          } else {
-            this.setState({ dailyFact: cleanAstronomyData(dailyFact) })
-          }
-        })
-        .catch(err => err.message)
-    }
-
-    render() {
-      return (
-        <>
-        {this.state.error &&
-            <h2>{this.state.error}</h2>
+  componentDidMount () {
+    fetchDailyFact()
+      .then(dailyFact => {
+        if(typeof dailyFact === 'string') {
+          this.setState({ error: dailyFact })
+        } else {
+          this.setState({ dailyFact: cleanAstronomyData(dailyFact) })
         }
-        {!this.props.isClicked && !this.state.error &&
-          <div className='main-astronomy-card'>
-            <div  className='card-border'>
-              <img className='main-astronomy-image' src='history-astro.png' alt='history of astronomy'/>
-              <h3 className='main-title'>..| Astronomy |..</h3>
-            </div>
+      })
+      .catch(err => err.message)
+  }
+
+  render() {
+    return (
+      <>
+      {this.state.error &&
+          <h2>{this.state.error}</h2>
+      }
+      {!this.props.isClicked && !this.state.error &&
+        <div className='main-astronomy-card'>
+          <div  className='card-border'>
+            <img className='main-astronomy-image' src='history-astro.png' alt='history of astronomy'/>
+            <h3 className='main-title'>..| Astronomy |..</h3>
           </div>
-        }
-        {this.props.isClicked && !this.state.error &&
-          <AstronomyFact
-            dailyFact={this.state.dailyFact}
-            saveFact={this.props.saveFact}
-          />
-        }
-        </>
-      )
-    }
+        </div>
+      }
+      {this.props.isClicked && !this.state.error &&
+        <AstronomyFact
+          dailyFact={this.state.dailyFact}
+          saveFact={this.props.saveFact}
+        />
+      }
+      </>
+    )
+  }
 }
 
 export default Astronomy;
