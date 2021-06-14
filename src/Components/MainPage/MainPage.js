300d81899f303e496d0db47d107de3e2618ad856
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 02:14:30 -0600

    Remove unused import

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Astronomy from '../Astronomy/Astronomy';
 import Astrology from '../Astrology/Astrology';
 
