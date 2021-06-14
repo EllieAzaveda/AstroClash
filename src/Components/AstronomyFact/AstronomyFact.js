39f947f5fe089e1e89b718b25b597e4bbe5c8955
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 13:41:19 -0600

    Add additional class names

--- src/Components/AstronomyFact/AstronomyFact.js
+++ src/Components/AstronomyFact/AstronomyFact.js
@@ -3,7 +3,7 @@ import './AstronomyFact.css';
 
 const AstronomyFact = ({ dailyFact, saveFact }) => {
   return (
-    <div className='astronomy-card'>
+    <div className='astronomy-card card'>
       <img data-cy='astronomy-image' className='astronomy-image' src={dailyFact.url} alt={`${dailyFact.title}`}/>
       <h3 data-cy='title' className='title'>..| {dailyFact.title} |..</h3>
       <h5 data-cy='explanation' className='explanation'>{dailyFact.explanation}</h5>
