93ca3d758f873a1cce5c2ad9db3ad9383bb10197
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 11:53:03 -0600

    Change back button text

--- src/Components/NotFoundPage/NotFoundPage.js
+++ src/Components/NotFoundPage/NotFoundPage.js
@@ -4,10 +4,12 @@ import './NotFoundPage.css';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <NavLink to='/'><button data-cy='home-button' className='home-button'>Back to Home Page</button></NavLink>
-      <img className='error-img' src="404Error.jpeg"  alt="page not found purple pink and green 404 in circle" style={{textAlign:"center"}} />
-    </div>
+    <>
+      <NavLink to='/'><button data-cy='home-button' className='home-button'>Take Me Back Home!</button></NavLink>
+      <div>
+        <img className='error-img' src="404Error.jpeg"  alt="page not found purple pink and green 404 in circle" style={{textAlign:"center"}} />
+      </div>
+    </>
   )
 }
 
