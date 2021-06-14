38b044806bb1e100feeda3807612b7a8621e19e3
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 15:11:01 -0600

    Fix subtitle header to pass lighthouse audit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = ({ displaySavedFacts, displaySavedScopes }) => {
   return (
     <nav className='header'>
       <h1 className='header-title'>AstroClash</h1>
-      <h4 className='subtitle'>WHERE YOU DON'T NEED TO KNOW THE DIFFERENCE BETWEEN ASTRONOMY & ASTROLOGY</h4>
+      <h2 className='subtitle'>WHERE YOU DON'T NEED TO KNOW THE DIFFERENCE BETWEEN ASTRONOMY & ASTROLOGY</h2>
     </nav>
   )
 }
