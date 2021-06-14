2e972b16923dbcc15076ff0075de4e04fbb97164
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 01:24:05 -0600

    Add classname to image tag

--- src/Components/NotFoundPage/NotFoundPage.js
+++ src/Components/NotFoundPage/NotFoundPage.js
@@ -6,7 +6,7 @@ const NotFoundPage = () => {
   return (
     <div>
       <NavLink to='/'><button data-cy='home-button' className='home-button'>Back to Home Page</button></NavLink>
-      <img src={'404Error.jpeg'}  alt="page not found purple pink and green 404 in circle" style={{textAlign:"center"}} />
+      <img className='error-img' src="404Error.jpeg"  alt="page not found purple pink and green 404 in circle" style={{textAlign:"center"}} />
     </div>
   )
 }
