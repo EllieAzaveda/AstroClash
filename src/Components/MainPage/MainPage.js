7a331cedeb1a7166413bcb040941441fabbbf7f6
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 19:38:02 -0600

    Add proptype checking

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import Astronomy from '../Astronomy/Astronomy';
 import Astrology from '../Astrology/Astrology';
 
@@ -28,5 +29,13 @@ const MainPage = ({ saveFact, selectedSign, isClicked, saveScope, error }) => {
     </>
   )
 }
+
+MainPage.propTypes = {
+  saveFact: PropTypes.func,
+  selectedSign: PropTypes.object,
+  isClicked: PropTypes.bool,
+  saveScope: PropTypes.func,
+  error: PropTypes.string
+};
 
 export default MainPage;
