80ffab4a21b574a336e0a1b94bc83816b66f75b5
EllieAzaveda <elliemiller66@icloud.com>
2021-06-14 19:05:20 -0600

    Remove console logs

--- src/Components/AstrologyCard/AstrologyCard.js
+++ src/Components/AstrologyCard/AstrologyCard.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { fetchSingleHoroscope } from '../../Utils/APICalls';
 import { cleanHoroscopeData } from '../../Utils/cleaningMethods';
 
+
 class AstrologyCard extends Component {
   constructor(props){
       super(props);
